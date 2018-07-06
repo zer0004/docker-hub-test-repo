93e1b0032c11c1b92600ec23778171fe0e94f720
EC2 Default User <ec2-user@ip-172-31-45-133.ec2.internal>
2018-07-06 13:16:05 +0000

    mod python parenthesis

--- sample_bot.py
+++ sample_bot.py
@@ -8,11 +8,11 @@ url = "http://chanz.jp/bots/insertForwardTest.php"
 
 pos = "buy"
 while True:
-	time.sleep(10)
+	time.sleep(60)
 	s = requests.session()
 	params = {"user_id":"user_id2", "bot_id":"bot_id2", "market_name":"bitFlyer","position":pos}
 	r =  s.post(url, data=params)
-	print params
+	print(params)
 	if pos == "buy":
 		pos = "sell"
 	else:
