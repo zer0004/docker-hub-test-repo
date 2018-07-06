ad77bdcdf5a42d7915735bd624ab95cb889a975a
EC2 Default User <ec2-user@ip-172-31-45-133.ec2.internal>
2018-07-06 12:58:23 +0000

    add bot logic

--- sample.py
+++
@@ -1,2 +0,0 @@
-print ("hello docker from python")
-print ("another line")
---
+++ sample_bot.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import time
+
+url = "http://chanz.jp/bots/insertForwardTest.php"
+
+pos = "buy"
+while True:
+	time.sleep(10)
+	s = requests.session()
+	params = {"user_id":"user_id2", "bot_id":"bot_id2", "market_name":"bitFlyer","position":pos}
+	r =  s.post(url, data=params)
+	print params
+	if pos == "buy":
+		pos = "sell"
+	else:
+		pos = "buy"
