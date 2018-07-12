8d6b50575005f0c2db188e64c62c36ac2febb421
EC2 Default User <ec2-user@ip-172-31-45-133.ec2.internal>
2018-07-12 11:42:30 +0000

    fix format

--- sample_bot.py
+++ sample_bot.py
@@ -4,23 +4,28 @@
 import requests
 import time
 import json
+import sys
 
 url = "https://25b4v0hwdg.execute-api.us-east-2.amazonaws.com/development/sendPseudoOrder"
 
 pos = "buy"
 while True:
 	s = requests.session()
+	headers = {"Content-Type" : "application/json"}
 	params = {
-		"BotId": "ec2_local",
+		"BotId": "ec2_local_second",
   		"MarketName": "bitFlyer",
  	 	"Position": pos,
   		"Product": "FX_BTC_JPY"
 	}
-	r =  s.post(url, data=params)
-	print(json.dumps(params))
+	json_data = json.dumps(params).encode("utf-8")
+	r =  s.post(url, data=json_data)
+	print(params)
 	print(r)
+	sys.stdout.flush()
 	if pos == "buy":
 		pos = "sell"
 	else:
 		pos = "buy"
+	break
 	time.sleep(60)
